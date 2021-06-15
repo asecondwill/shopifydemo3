fe3c63c6a647f9b591b927b60a6e0b5391283217
Will <will@kindleman.com.au>
2021-06-15 15:00:19 +0100

    trying this change.  it makes no difference

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Shopifydemo3
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    config.action_dispatch.cookies_same_site_protection = :none
 
     # Configuration for the application, engines, and railties goes here.
     #
