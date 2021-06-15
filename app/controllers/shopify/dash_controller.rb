00ad833fa67d0d621fc78c7b0d8eb0a56e7b2d49
Will <will@kindleman.com.au>
2021-06-15 11:23:23 +0100

    setup as per real app.  authentication not sticking.

---
+++ app/controllers/shopify/dash_controller.rb
@@ -0,0 +1,14 @@
+class Shopify::DashController < ApplicationController
+  include ShopifyApp::EmbeddedApp
+  include ShopifyApp::RequireKnownShop
+  include ShopifyApp::ShopAccessScopesVerification
+  layout 'shopify'
+  def dash
+    @shop_origin = current_shopify_domain
+    @host = params[:host]
+  end
+
+  private
+
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  root :to => 'home#index'
+
+
+  mount ShopifyApp::Engine, at: '/shopify'
+  namespace :shopify do
+    get "dash", to: "dash#dash"
+    get '/products', :to => 'products#index'
+  end
+
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
