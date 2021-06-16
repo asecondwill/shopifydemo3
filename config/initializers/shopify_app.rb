6e629c186be391a9dc1b7a8e6d5a058efed0ac03
Will <will@kindleman.com.au>
2021-06-16 15:22:58 +0100

    according to tickets for other issues, jwt is best, and cookies is a fallback.  so enabled both.

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -11,7 +11,7 @@ ShopifyApp.configure do |config|
   config.reauth_on_access_scope_changes = true
 
   config.allow_jwt_authentication = true
-  config.allow_cookie_authentication = false
+  config.allow_cookie_authentication = true
 
   config.api_key = ENV.fetch('SHOPIFY_API_KEY', '').presence
   config.secret = ENV.fetch('SHOPIFY_API_SECRET', '').presence
